1c52098215c2747d0443c63f4d8fc97b04f2de93
Kétcia Mota <160603158+ketciamota@users.noreply.github.com>
2024-03-14 14:26:24 +0000

    criando operadores de atribuição

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,20 @@
+const numero1 = 40;
+const numero2 = 20;
+
+const soma = numero1 + numero2;
+
+const subtracao = numero1 - numero2;
+
+const multiplicacao = numero1 + numero2;
+
+const divisao = numero1 / numero2;
+
+const resto = numero1 % numero2;
+
+// copiar linhas: alt + shift + 1
+console.log(`Os numeros das operações são ${numero1} é ${numero2}`);
+console.log(`A soma dos numeros é ${numero1} + ${numero2}`);
+console.log(`A subtração dos numeros é ${numero1} - ${numero2}`);
+console.log(`A multiplicação dos numeros é ${numero1} * ${numero2}`);
+console.log(`A divisão dos numeros é ${numero1} / ${numero2}`);
+console.log(`O resto dos numeros é ${numero1} $ ${numero2}`)
---
+++ operadores-atribuicao.js
@@ -0,0 +1,14 @@
+let numero = 10;
+
+console.log(`O número é inicialmente igual a ${numero}`);
+
+numero += 10;
+
+console.log(`Após atribuir somando o número 10: ${numero +=10}`);
+console.log(`Reatribuindo e subtraindo 10: ${numero -=10}`);
+console.log(`Reatribuindo e multiplicando 10: ${numero *=10}`);
+console.log(`Reatribuindo e dividindo 10: ${numero /=10}`);
+console.log(`Reatribuindo para o resto da divisão por 10:${numero}`);
+
+
+
