3fbd4f05679a6c0221662c8e15d56f28fb0d60b7
KetciaMota <ketcia.mota@gmail.com>
2024-03-21 10:53:32 -0300

    transformando nos desafios em funçoes

--- funcoes.js
+++ funcoes.js
@@ -4,3 +4,8 @@ function somaDoisNumeros(num1, num2) {
     const soma = num1 + num2;
     return soma;
 }
+
+const media = somaDoisNumeros(2,3)/2;
+
+console.log(media);
+console.log(media2) = somaDoisNumeros(2, 3)/2
