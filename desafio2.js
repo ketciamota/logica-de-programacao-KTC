2ee2230f0260d77d4fa73e72e9b2a42cc95e512f
KetciaMota <ketcia.mota@gmail.com>
2024-03-21 11:56:39 -0300

    Adaptando a calculadora IMC

--- desafio2.js
+++ desafio2.js
@@ -1,26 +1,26 @@
-function calculeimc() {
+function calculeimc(peso, altura) {
 
-    const 
-        peso = window.prompt(`Digite seu peso:`);
-        altura = window.prompt(`Digite sua altura`);
-        imc = peso / altura ** 2;
+    const imc = peso / altura ** 2;
+    let mensagem;
     
     if(imc < 18.5) {
         console.log(`IMC: ${imc}) -- Abaixo do peso`);
     
     }else if(imc > 18.5 && imc < 24.9) {
-        window.alert(`IMC: ${imc}) -- Peso normal`);
+        mensagem(`IMC: ${imc}) -- Peso normal`);
     
     }else if(imc >= 25.0 && imc < 30.0) {
-        window.alert(`IMC: ${imc}) -- Sobrepeso`);
+        mensagem(`IMC: ${imc}) -- Sobrepeso`);
     
     }else if(imc >=30.0 && imc < 35.0) {
-        window.alert(`IMC ${imc}) -- Obesidade grau I`);
+        mensagem(`IMC ${imc}) -- Obesidade grau I`);
     
     }else if(imc >=35 && imc < 40) {
-        window.alert(`iMC ${imc}) -- Obesidade grau II`);
+        mensagem(`iMC ${imc}) -- Obesidade grau II`);
     
     }else {
-        window.alert(`IMC ${imc} -- Obesidade grau III`)`;`
+        mensagem`IMC: ${imc.toFixed()} -- Obesidade grau III`)`;`
     }
+
+    return mensagem;
 }
