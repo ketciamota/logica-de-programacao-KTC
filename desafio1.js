c07471fe002a715e99a4f847c5f513d291e1ee32
ketciamota <ketcia.mota@gmail.com>
2024-05-07 10:57:43 -0300

    refact: ajustes gerais

--- desafio1.js
+++ desafio1.js
@@ -1,16 +1,13 @@
 const
+    nota1 = 3,
+    nota2 = 0,
+    nota3 = 7,
+    media = (nota1 + nota2 + nota3) / 3;
 
-
-
-
-
-
-if (media >= 7)
-    console.log(O aluno foi APROVADO com media ${ media.toFixed} ) {
- 
+if (media >= 7) {
+    console.log(`O aluno foi APROVADO com média ${media.toFixed(2)}`);
 } else if (media >= 5) {
-    console.log(O aluno está de EXAME com media )
-  
+    console.log(`O aluno está de EXAME com média ${media.toFixed(2)}`);
 } else {
-    
+    console.log(`O aluno foi REPROVADO com média ${media.toFixed(2)}`);
 }
