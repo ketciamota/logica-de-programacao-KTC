a754c8126c6d80a5da259c6a499c9fabb56b38db
ketciamota <ketcia.mota@gmail.com>
2024-05-08 08:55:32 -0300

    refact: resolução desafio 2 IMC

--- desafio2.js
+++ desafio2.js
@@ -1,26 +1,27 @@
-function calculeimc(peso, altura) {
+function calculaIMC(peso, altura) {
 
     const imc = peso / altura ** 2;
     let mensagem;
+        
+    if (imc < 18.5) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Abaixo do peso`;
     
-    if(imc < 18.5) {
-        mensagem = `IMC: ${imc.toFixed(1)} -- Abaixo do peso`;
+    } else if (imc < 25) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Peso normal`;
     
-    }else if (imc < 25) {
-        mensagem = `IMC: ${imc.toFixed(1)} -- Peso normal`;
+    } else if (imc < 30) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Sobrepeso`;
     
-    }else if(imc < 30) {
-        mensagem = `IMC: ${imc.toFixed(1)} -- Sobrepeso`;
+    } else if (imc < 35) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau I`;
     
-    }else if(imc < 35) {
-        mensagem = `IMC ${imc.toFixed(1)} -- Obesidade grau I`;
+    } else if (imc < 40) {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau II`;
     
-    }else if( imc < 40) {
-        mensagem = `iMC ${imc.toFixed(1)} -- Obesidade grau II`;
-    
-    }else {
-        mensagem = `IMC: ${imc.toFixed(1)} -- Obesidade grau III`;
+    } else {
+        mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau III`;
     }
 
     return mensagem;
+
 }
